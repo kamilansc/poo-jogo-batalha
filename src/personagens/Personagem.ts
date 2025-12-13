de1bedfd7ff9cbb696bc21b807a75669552cbd5d
kamilansc <kamilarocha155@gmail.com>
2025-12-13 12:57:53 -0300

    feat: cria classe base Personagem

---
+++ src/personagens/Personagem.ts
@@ -0,0 +1,44 @@
+export default class Personagem {
+    private _id: number;
+    private _nome: string;
+    private _vida: number;
+    private _ataque: number;
+    private _historico: Acao[];
+
+    constructor(id: number, nome: string, vida: number, ataque: number) {
+        this._id = id;
+        this._nome = nome;
+        this._vida = this.validarVida(vida);
+        this._ataque = ataque;
+        this._historico = [];
+    }
+
+    validarVida(vida: number) {
+        if (vida >= 0 && vida <= 100) {
+            return vida;
+        }
+        throw new Error("Vida inválida");
+    }
+
+    /* ======== FUNCIONALIDADES ======== */ 
+    atacar(alvo: Personagem): Acao {
+        alvo.receberDano(this._ataque);
+    }
+
+    receberDano(valor: number): void {
+        if (this._vida >= valor) {
+            this._vida -= valor;
+            return;
+        }
+        
+        this._vida = 0;
+    }
+
+    estaVivo(): boolean {
+        return this._vida > 0;
+    }
+
+    registrarAcao(acao: Acao): void {
+        this._historico.push(acao);
+    }
+}
